0eb22969d4f2a8c970bcab483c8c9a9fde50ae0f
Arthur-codeX <daniel.karanja239@gmail.com>
2025-08-04 21:53:19 +0300

    updated permission

--- CMD/GenCrudModel/permission.js
+++ CMD/GenCrudModel/permission.js
@@ -6,6 +6,10 @@ const getPermission = (type) => {
 
   const permission = `
 
+/*
+
+*/
+
 function bf1(req){
   /*
     if this bf1 function throws an error 
@@ -14,10 +18,20 @@ function bf1(req){
     return
 }
 
+
 const permission={
    allowedMethods:["GET", "POST", "PUT", "PATCH", "DELETE"],
-   beforeOperation:[bf1], // all your functions for before db change
-   afterOperation:[bf1]  // all your functions after db changes are made
+   beforeOperation:[], // This are before any operation ie [GET,POST,PUT,PATCH,DELETE]
+   afterOperation:[], // This are before any operation ie [GET,POST,PUT,PATCH,DELETE]
+   beforeGet:[bf1], // all your functions for before db change
+   afterGet:[bf1], // all your functions for before db change
+   beforePut:[bf1], // all your functions for before db change
+   afterPut:[bf1], // all your functions for before db change
+   beforePatch:[bf1], // all your functions for before db change
+   afterPatch:[bf1], // all your functions for before db change
+   beforeDelete:[bf1], // all your functions for before db change
+   afterDelete:[bf1], // all your functions for before db change
+ 
  }
 
 ${exportString}
